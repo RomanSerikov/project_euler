8e03eee03db26ee79b9941578b58bc4941795b3c
Roman Serikov <roman.serikov@yandex.ru>
2017-12-29 18:46:06 +0300

    Add: solution for problem #44

---
+++ 44.rb
@@ -0,0 +1,40 @@
+# № 44 — Пятиугольные числа.
+# Пятиугольные числа вычисляются по формуле: Pn = n * (3n - 1) / 2.
+# Первые десять пятиугольных чисел:
+
+#   1, 5, 12, 22, 35, 51, 70, 92, 117, 145, ...
+
+# Можно убедиться в том, что P4 + P7 = 22 + 70 = 92 = P8
+# Однако, их разность, 70 - 22 = 48 не является пятиугольным числом.
+
+# Найдите пару пятиугольных чисел Pj и Pk, для которых сумма и разность
+# являются пятиугольными числами и значение D = |Pk - Pj| минимально,
+# и дайте значение D в качестве ответа.
+
+def make_pentagon_number(n)
+  n * (3 * n - 1) / 2
+end
+
+def isPentagon?(n)
+  ( 1 + Math.sqrt(24 * n + 1) ) % 6 == 0
+end
+
+def find_pentagon
+  pentagon_numbers = []
+
+  (1..10_000).each do |i|
+    pentagon_numbers << make_pentagon_number(i)
+  end
+
+  pentagon_numbers.each do |a|
+    pentagon_numbers.each do |b|
+      next if a <= b
+      next unless isPentagon?(a - b)
+      next unless isPentagon?(a + b)
+
+      return a - b
+    end
+  end
+end
+
+puts "Diff: #{find_pentagon}"
